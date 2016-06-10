728361a0c1dd8f145b571c75b828afeaf381470d
ace-yang <ace_yang@sina.com>
2016-06-10 20:33:54 +0800

    initial version

---
+++ src/Test/Test1.py
@@ -0,0 +1,16 @@
+'''
+Created on 2016Äê6ÔÂ10ÈÕ
+
+@author: nsnp200
+'''
+
+class MyClass(object):
+    '''
+    This a test
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
